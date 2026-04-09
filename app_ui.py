8606a6e36a7c8b10b18b227adec68e05934aa7ce
nhjin0nhjin0524-hu <nhjin0nhjin0524@gmail.com>
2026-04-09 16:17:53 +0900

    Update app_ui.py

--- app_ui.py
+++ app_ui.py
@@ -38,6 +38,7 @@ def get_db_connection():
         db='cooking_db',
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
+		ssl_verify_cert=False
     )
 
 # 💡 [스마트 단위 추론기] 식재료 이름을 보고 가장 자연스러운 단위를 찰떡같이 찾아냅니다!
